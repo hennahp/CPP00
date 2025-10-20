13e57474c545924065f1df6ddcd860e8f48cdfe6
hennahp <hennaparveen77@gmail.com>
2025-10-20 21:08:55 +0400

    started ex01

---
+++ ex01/Contact.hpp
@@ -0,0 +1,19 @@
+#ifndef CONTACT_HPP
+#define CONTACT_HPP
+
+#include <string>
+
+class Contact {
+private:
+    std::string firstName;
+    std::string lastName;
+    std::string nickname;
+    std::string phoneNumber;
+    std::string darkestSecret;
+public:
+    Contact();
+    ~Contact();
+
+
+
+#endif
