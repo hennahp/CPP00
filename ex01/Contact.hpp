9f9c719e70f0d7aa42a57b93f7f812cf2fa307f4
hennaparveen <hennaparveen77@gmail.com>
2025-10-22 11:27:07 +0400

    done

--- ex01/Contact.hpp
+++ ex01/Contact.hpp
@@ -13,7 +13,19 @@ private:
 public:
     Contact();
     ~Contact();
+   
+    std::string getFirstName() const;
+    std::string getLastName() const;
+    std::string getNickname() const;
+    std::string getPhoneNumber() const;
+    std::string getDarkestSecret() const;
 
+    void setFirstName(const std::string &firstName);
+    void setLastName(const std::string &LastName);
+    void setNickname(const std::string &nickname);
+    void setPhoneNumber(const std::string &phoneNumber);
+    void setDarkestSecret(const std::string &darkestSecret);
+};
 
 
 #endif
