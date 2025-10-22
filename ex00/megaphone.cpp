2e0f820222f8eb9ec457d64c20f335bf75bf92d9
hennaparveen <hennaparveen77@gmail.com>
2025-10-22 10:13:37 +0400

    ex00 done

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hparveen <hparveen@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/22 09:53:29 by hparveen          #+#    #+#             */
+/*   Updated: 2025/10/22 10:09:15 by hparveen         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <iostream>
 #include <string>
 #include <cctype>
@@ -9,8 +21,10 @@ int main(int ac, char **av)
     else
     {
         for(int i = 1 ; i < ac; i++)
-           for(int j = 0; av[i][j] != '\0'; j++)
-               std::cout << (char)std::toupper((unsigned char)av[i][j]);
+        {
+            for(int j = 0; av[i][j] != '\0'; j++)
+                std::cout << (char)std::toupper((unsigned char)av[i][j]);
+        }
         std::cout << std::endl;
     }
     return 0;
